6e05ab37c9d3610e370ecfe08930190a42647975
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-11-29 15:36:09 +0530

    Add files via upload
    react assignment 1

---
+++ git/app.js
@@ -0,0 +1,4 @@
+var parent= [React.createElement("h1",{},"HELLO WORLD"),React.createElement("h2",{},"welcome to react"),React.createElement("h3",{},"using js")]
+var htmlRoot=document.getElementById("root")
+var rootReact=ReactDOM.createRoot(htmlRoot)
+rootReact.render(parent)
